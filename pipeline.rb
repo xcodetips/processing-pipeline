775adcf1f66761a971a85a156a861d71f9fb79c7
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-03-28 21:33:01 +1100

    Add poster generation and ensure resize

--- pipeline.rb
+++ pipeline.rb
@@ -3,22 +3,28 @@ require 'tmpdir'
 input_path = ARGV[0] || Dir.pwd
 output_path = ARGV[1] || Dir.pwd
 
+@desired_width = 1280
+
 def timestamp
   Time.now.to_i
 end
 
-def convert_to_mp4(input_path, output_path)
-  `ffmpeg -an -i #{input_path} -vf "scale=1280:-2:flags=lanczos" -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 #{output_path}`
+def extract_cover(input_path, output_path, width = @desired_width)
+  `ffmpeg -ss 00:00:00 -i #{input_path} -vf "scale=#{width}:-2:flags=lanczos" -vframes 1 -q:v 1 #{output_path}`
 end
 
-def convert_to_gif(input_path, output_path)
+def convert_to_mp4(input_path, output_path, width = @desired_width)
+  `ffmpeg -an -i #{input_path} -vf "scale=#{width}:-2:flags=lanczos" -vcodec libx264 -pix_fmt yuv420p -profile:v baseline -level 3 #{output_path}`
+end
+
+def convert_to_gif(input_path, output_path, width = @desired_width)
   Dir.mktmpdir do |tmp_dir|
     # Convert to GIF
     raw_path = "#{tmp_dir}/#{timestamp}.gif"
     `ffmpeg -i #{input_path} -r 10 #{raw_path}`
 
     # Optimize GIF
-    `gifsicle #{raw_path} --no-interlace --careful --no-comments --no-names --same-delay --same-loopcount --no-warnings -O3 --colors=48 --use-col=web > #{output_path}`
+    `gifsicle #{raw_path} --resize-width #{width} --no-interlace --careful --no-comments --no-names --same-delay --same-loopcount --no-warnings -O3 --colors=48 --use-col=web > #{output_path}`
   end
 end
 
@@ -34,4 +40,5 @@ inputs.each do |mov|
 
   convert_to_mp4(mov, "#{output_path}/#{basename}.mp4")
   convert_to_gif(mov, "#{output_path}/#{basename}.gif")
+  extract_cover(mov, "#{output_path}/#{basename}.jpg")
 end
